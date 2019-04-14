9f2e7adadcc0ea4869476d9e89851d11066be41d
William Zhang <whzhang@umich.edu>
2019-04-14 19:32:47 -0400

    Updates

--- Keys.py
+++ Keys.py
@@ -9,9 +9,4 @@ ZomatoAPI = "92f52707cb7d5d28d57b18760216faf5"
 NutritionixAPI = "03e16980"
 NutriotinixAuth = "6119e5f6c1deef7767e839739e9f04b1"
 
-nutritionix_key = Keys.nutritionix_key
-nutritionix_access_token = Keys.nutritionix_access_token
 
-
-auth = tweepy.OAuthHandler(cosnsumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
--- NutritionIX.py
+++ NutritionIX.py
@@ -1,3 +1,9 @@
 import requests
 import json
 import Keys
+
+nutritionixAPI = Keys.nutritionixAPI
+nutritionixAuth= Keys.nutritionixAuth
+
+auth = tweepy.OAuthHandler(cosnsumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)

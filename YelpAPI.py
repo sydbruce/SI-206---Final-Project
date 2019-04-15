b055936ed4da274531d7ff54cb5e64f5cd446951
Sydney Bruce <sydbruce@umich.edu>
2019-04-15 12:42:40 -0400

    GotInfo

--- YelpAPI.py
+++ YelpAPI.py
@@ -4,3 +4,11 @@ import Keys
 
 Yelp_ClientID = Keys.client_ID
 YelpAPIKey = Keys.API_Key
+headers = {'Authorization': 'Bearer %s' % YelpAPIKey}
+url = 'https://api.yelp.com/v3/businesses/search'
+
+params = {'term': 'food', 'location': 'Ann Arbors'}
+req = requests.get(url,params=params, headers=headers)
+print('The status code is {}'.format(req.status_code))
+print(json.loads(req.text))
+

fd1462bc48678f0644f407ca21d40484cf03b5a4
Dylan Rabin <dmrabin@umch.edu>
2019-04-15 16:46:01 -0400

    Fetch Zomato categories

--- ZomatoAPI.py
+++ ZomatoAPI.py
@@ -2,5 +2,11 @@
 import requests
 import json
 
-#Fill these in in the Keys.py file
+
 zomato_key = Keys.ZomatoAPI
+url = "https://developers.zomato.com/api/v2.1/categories"
+
+params = {'user-key': zomato_key}
+req = requests.get(url,params=params)
+print('The status code is {}'.format(req.status_code))
+print(json.loads(req.text))

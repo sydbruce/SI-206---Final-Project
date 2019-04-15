32586ab013ce8e0d6da46371dfca3591819415bb
William Zhang <whzhang@umich.edu>
2019-04-15 14:21:07 -0400

    Update

--- NutritionIX.py
+++ NutritionIX.py
@@ -1,13 +1,20 @@
 import requests
 import json
 import Keys
+import sqlite3
 
 #Nutritionix Key Configuration
+
 appID = Keys.appID
 appKey = Keys.appKey
 
 from nutritionix import Nutritionix
 nix = Nutritionix(app_id = appID, api_key = appKey)
 
-def get_nutrition_info(api, cacheDict, fname):
-    print(nix.search('Big Mac').json())
+def get_nutrition_info(food_item):
+    return nix.search(food_item).json()
+
+print(get_nutrition_info("Big Mac"))
+print(get_nutrition_info("Pizza"))
+
+#def setupDatabase()

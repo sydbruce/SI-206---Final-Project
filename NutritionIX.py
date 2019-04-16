7b93a74236ccb435de42b3450c88e0c4aa290378
William Zhang <whzhang@umich.edu>
2019-04-16 15:50:42 -0400

    Update NutritionIX.py

--- NutritionIX.py
+++ NutritionIX.py
@@ -8,13 +8,17 @@ import sqlite3
 appID = Keys.appID
 appKey = Keys.appKey
 
+def SQLSetup():
+    conn = sqlite3.connect("NutritionixData.sql")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE ")
+
+
 from nutritionix import Nutritionix
 nix = Nutritionix(app_id = appID, api_key = appKey)
 
 def get_nutrition_info(food_item):
     return nix.search(food_item).json()
 
-print(get_nutrition_info("Big Mac"))
-print(get_nutrition_info("Pizza"))
-
-#def setupDatabase()
+def save_info(data):
+    

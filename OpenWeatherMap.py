8f1a8f41c09fb2fef6cd45f3e7177c31878832bd
William Zhang <whzhang@umich.edu>
2019-04-21 18:18:41 -0400

    Update OpenWeatherMap.py

--- OpenWeatherMap.py
+++ OpenWeatherMap.py
@@ -2,6 +2,7 @@ import requests
 import json
 import Keys
 import sqlite3
+#pprint allows for the "prettified" printing of json paragraphs so that they can be read by humans
 import pprint
 
 #database configuration
@@ -9,16 +10,15 @@ def SQLSetup():
     conn = sqlite3.connect("NutritionixData.sql")
     cur = conn.cursor()
     cur.execute("CREATE TABLE ")
-    
 
-api_key = Keys.api_key
-base_url = "http://api.openweathermap.org/data/2.5/forecast?id="
-city_id = input("Enter city id : ") 
-complete_url = base_url + city_id + "&appid=" + api_key
-raw_response = requests.get(complete_url) 
-response = raw_response.json()
 
-pprint.pprint(response)
+def getWeather(city_id):
+    api_key = Keys.api_key
+    base_url = "http://api.openweathermap.org/data/2.5/forecast?id=" 
+    complete_url = base_url + city_id + "&appid=" + api_key
+    raw_response = requests.get(complete_url) 
+    response = raw_response.json()
+    pprint.pprint(response)
 
 LA = "5368381"
 #if response["cod"] != "404": 

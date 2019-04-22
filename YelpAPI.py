e598fc0e4ec67bfc6848e735102c256cbf87730d
Sydney Bruce <sydbruce@umich.edu>
2019-04-22 14:59:16 -0400

    Fixed

--- YelpAPI.py
+++ YelpAPI.py
@@ -6,15 +6,13 @@ import sqlite3
 Yelp_ClientID = Keys.client_ID
 YelpAPIKey = Keys.API_Key
 
-def getYelp(YelpAPIKey):
-    for i in range(5):
-        search_city = input("Enter a city in the United States: ")
-        headers = {'Authorization': 'Bearer %s' % YelpAPIKey}
-        url = 'https://api.yelp.com/v3/businesses/search'
-        params = {'term': 'hotel', 'location': search_city}
-        req = requests.get(url,params=params, headers=headers)
-        print('The status code is {}'.format(req.status_code))
-        return json.loads(req.text)
+def getYelp(YelpAPIKey, search_city):
+    headers = {'Authorization': 'Bearer %s' % YelpAPIKey}
+    url = 'https://api.yelp.com/v3/businesses/search'
+    params = {'term': 'hotel', 'location': search_city}
+    req = requests.get(url,params=params, headers=headers)
+    print('The status code is {}'.format(req.status_code))
+    return (json.loads(req.text))
 
 
 
@@ -22,8 +20,8 @@ def setupYelpDataBase(YelpList):
     conn = sqlite3.connect('YelpData.sqlite')
     cur = conn.cursor()
 
-    cur.execute('CREATE TABLE IF NOT EXISTS YelpData(id TEXT UNIQUE, name TEXT, review_count INTEGER, rating FLOAT, price STRING, location TEXT)')
-
+    cur.execute('CREATE TABLE IF NOT EXISTS YelpData(id TEXT UNIQUE, name TEXT, review_count INTEGER, rating FLOAT, price STRING, location TEXT, city TEXT)')
+    counter = 0
     for yelp in YelpList['businesses']:
         _ID = yelp['id']
         _name = yelp['name']
@@ -34,11 +32,34 @@ def setupYelpDataBase(YelpList):
         except:
             _price = ' '
         _location = yelp['location']['address1']
+        _city = yelp['location']['city']
+        counter += 1
+        if counter == 11:
+            break
 
-        cur.execute('INSERT OR IGNORE INTO YelpData(id, name, review_count, rating, price, location) VALUES (?,?,?,?,?,?)', (_ID, _name, _review_count, _rating, _price, _location))
+        cur.execute('INSERT OR IGNORE INTO YelpData(id, name, review_count, rating, price, location, city) VALUES (?,?,?,?,?,?,?)', (_ID, _name, _review_count, _rating, _price, _location, _city))
         conn.commit()
-        cur.execute('SELECT * FROM YelpData LIMIT 100')
+        
 
-data = getYelp(YelpAPIKey)
-setupYelpDataBase(data)
+data1 = getYelp(YelpAPIKey, "Atlanta")
+data2 = getYelp(YelpAPIKey, "Boston")
+data3 = getYelp(YelpAPIKey, "Chicago")
+data4 = getYelp(YelpAPIKey, "Detroit")
+data5 = getYelp(YelpAPIKey, "Houston")
+data6 = getYelp(YelpAPIKey, "Los Angeles")
+data7 = getYelp(YelpAPIKey, "New York City")
+data8 = getYelp(YelpAPIKey, "Philadelphia")
+data9 = getYelp(YelpAPIKey, "San Francisco")
+data10 = getYelp(YelpAPIKey, "Seattle")
+setupYelpDataBase(data1)
+setupYelpDataBase(data2)
+setupYelpDataBase(data3)
+setupYelpDataBase(data4)
+setupYelpDataBase(data5)
+setupYelpDataBase(data6)
+setupYelpDataBase(data7)
+setupYelpDataBase(data8)
+setupYelpDataBase(data9)
+setupYelpDataBase(data10)
+
 

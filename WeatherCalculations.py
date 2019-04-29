8a0d26aa68c447de269497c904a4962a332c4388
William Zhang <whzhang@umich.edu>
2019-04-28 20:10:59 -0400

    dddddddd

--- WeatherCalculations.py
+++ WeatherCalculations.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 def get_precipitation_counts(db_filename, city_name):
     conn = sqlite3.connect(db_filename)
     cur = conn.cursor()
-    dicts = {}
+    city = {}
     cur.execute("SELECT weather_desc FROM WeatherData WHERE city_name = " + city_name)
     for row in cur:
         weather = (row[0][0:20])
-        city_name[weather] = city_name.get(weather, 0) + 1
+        city[weather] = city.get(weather, 0) + 1
     
-    return dicts
-
+    return city
+hi = """
 def visualize(d):
     xvals = ["Atlanta", "Boston", "Chicago", "Detroit", "Houston", "LA", "NYC", "Philadelphia", "San_Francisco", "Seattle"] 
     yvals = [d["Atlanta"], d["Boston"], d["Chicago"], d["Detroit"], d["Houston"], d["LA"], d["NYC"], d["Philadelphia"], d["San_Francisco"], d["Seattle"]]
@@ -23,8 +23,12 @@ def visualize(d):
     plt.title("Number of Tweets per Weekday")
     plt.show()
     plt.savefig("Canvas Upload")
+"""
 
 AtlantaData = get_precipitation_counts("WeatherData.sql", city_name = "'Atlanta'")
-
+BostonData = get_precipitation_counts("WeatherData.sql", city_name = "'Boston'")
+ChicagoData = get_precipitation_counts("weatherData.sql", city_name = "'Chicago'")
 
 print(AtlantaData)
+print(BostonData)
+print(ChicagoData)
